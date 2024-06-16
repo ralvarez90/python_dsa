c24f75b3cb3decbd7084f1a71983a25dda9ea8a6
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-06-16 05:08:02 -0600

    Clase list

---
+++ _01_listas.py
@@ -0,0 +1,32 @@
+"""LISTAS
+
+Son colecciones de datos mutables que permiten almacenar elementos de manera
+indexada. Los elementos son indexados desde el 0 hasta el de su longitud - 1.
+"""
+import tools
+
+
+def showExample01():
+    someList = ['Geeks', 'For', 'Geeks']
+    print(someList, 'with type:', type(someList))
+
+
+def showExample02():
+    someList = [['Geeks', 'For'], ['Geeks']]
+    print(f'Size of items list: {len(someList)}')
+
+    for sublist in someList:
+        print(sublist)
+
+    print(f'Last item in {someList}')
+    print(tools.getLastItem(someList))
+
+
+def main():
+    showExample01()
+    showExample02()
+
+
+if __name__ == '__main__':
+    main()
+    input('\nPress any key to continue . . . ')
